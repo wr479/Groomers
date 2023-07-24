f7a5a6c2e6ca63c782e4f4fbcbc941d6066ad003
Andrey <normar00@mail.ru>
2023-07-24 17:39:43 +0400

    up

---
+++ scripts/app.js
@@ -0,0 +1,2 @@
+import { animation_dog } from "./modules/animation_dog.js";
+animation_dog()
---
+++ scripts/modules/animation_dog.js
@@ -0,0 +1,18 @@
+
+export function animation_dog(){
+
+    $('.types-grooming').hover(function(){
+        const img = $(this).attr('data-image')
+        changeImage(img)
+    }, resetImage())
+
+    function changeImage(newImage) {
+        var image = document.getElementById('image');
+        image.src = newImage;
+    }
+    
+    function resetImage() {
+        var image = document.getElementById('image');
+        image.src = 'images/animate-image/funny-dog.png';
+    }
+}
