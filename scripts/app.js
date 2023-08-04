01b7aa212e3bd0d0407c67936d4c900316b6b596
Oleg <normar00@mail.ru>
2023-08-04 14:25:24 +0400

    up

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,9 @@ import { animation_dog } from "./modules/animation_dog.js";
 import { popup } from "./modules/popup.js";
 import { validation } from "./modules/valisatoin.js";
 import { utils } from "./modules/utils.js";
+import { menu } from "./modules/menu.js";
 
+menu()
 animation_dog()
 popup()
 validation()
